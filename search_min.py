915fce91b16a3fd6acd3037949aaef2bfd984ee4
enriquemoralesvite <1412513@utp.edu.pe>
2024-12-19 19:02:37 -0500

    adding the sort list

--- search_min.py
+++ search_min.py
@@ -9,10 +9,11 @@ def SearchMinFromList(L):
             minValue = L[counter]
             position = counter + 1
         counter += 1  # Move to the next element
-    
-    return minValue, position
+    sortedList = sorted(L)
+    return minValue, position, sortedList
 
 # Example usage:
 L = [23, -4, 0, 73, -10, 13]
-minValue, position = SearchMinFromList(L)
+minValue, position, sortedList = SearchMinFromList(L)
 print("The minimum value is:", minValue, "is located in the position",position)
+print ("The sorted list is:", sortedList)
